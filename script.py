8df64ac1993dbe6cb983d528526c338b0f62d0c0
SumitKumar1307 <sk8861620@gmail.com>
2021-04-14 18:45:11 +0530

    small changes

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ from github import Github
 import sys
 
 project_name = sys.argv[1]
-g = Github("access_token")
+g = Github("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")
 user = g.get_user()
 user.create_repo(project_name)
